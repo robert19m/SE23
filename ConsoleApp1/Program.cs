08ed8aa52083b5f279a188c434d469231a8fa904
Mikey2356 <56921159+Mikey2356@users.noreply.github.com>
2022-10-17 10:06:28 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("My name is Michael Davis!");
+Console.WriteLine("My name is Mark Roberts!");
+
+
+Console.WriteLine("My name Michael Davis");
